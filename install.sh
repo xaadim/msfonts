32d75a401cc27a57d74a2d498215471939c7fdd6
Carlos Alexandro Becker <caarlos0@gmail.com>
2012-11-21 22:33:51 -0200

    install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
+echo "cloning font repository..."
 
+cd /tmp
+git clone git://github.com/caarlos0/msfonts.git
+
+echo "copying fonts to /usr/share/fonts/TTF/..."
+mkdir -p /usr/share/fonts/TTF/
+cp msfonts/fonts /usr/share/fonts/TTF/
+
+echo "updating all caches..."
+fc-cache -vf
+mkfontscale
+mkfontdir
+
+echo "done."
 
